eec24f9a8283f0c3b79f0c99e171b5d1c4144790
Greg <greg19850@gmail.com>
2023-03-28 12:24:25 +0100

    moved Navbar to separate compopnent

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import HeaderComponent from './components/HeaderComponent.vue'
-import { useUserStore } from './stores/user'
-
-const user = useUserStore()
-
-const handleLogOut = () => {
-  user.logUserOut()
-}
+import NavbarComponent from './components/NavbarComponent.vue'
 </script>
 
 <template>
   <header>
     <HeaderComponent />
-    <nav class="navbar">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/skinformation">Skinformation</RouterLink>
-      <RouterLink to="/support">Get Support</RouterLink>
-      <div v-if="user.isUserLoggedIn" class="user">
-        <RouterLink to="/profile">{{ user.login.firstName }} {{ user.login.lastName }}</RouterLink>
-        <button @click="handleLogOut">Log Out</button>
-      </div>
-      <RouterLink v-else to="/login">Login / Register</RouterLink>
-    </nav>
+    <NavbarComponent />
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-.navbar {
-  width: 100vw;
-  padding: 20px;
-  display: flex;
-  justify-content: flex-start;
-}
-
-.navbar a {
-  margin-right: 10px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/skinformation">Skinformation</RouterLink>
+    <RouterLink to="/support">Get Support</RouterLink>
+    <div v-if="user.isUserLoggedIn" class="user">
+      <RouterLink to="/profile">{{ user.login.firstName }} {{ user.login.lastName }}</RouterLink>
+      <button @click="handleLogOut">Log Out</button>
+    </div>
+    <RouterLink v-else to="/login">Login / Register</RouterLink>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { useUserStore } from '../stores/user'
+
+const user = useUserStore()
+
+const handleLogOut = () => {
+  user.logUserOut()
+}
+</script>
+
+<style scoped>
+.navbar {
+  width: 100vw;
+  padding: 20px;
+  display: flex;
+  justify-content: flex-start;
+}
+
+.navbar a {
+  margin-right: 10px;
+}
+</style>
